c17d1311ae460dc919c220638e8274df96bbdf7f
Rafael Moura <rs1674801@gmail.com>
2024-11-02 23:09:48 -0300

    Implementando rotas CRUD

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def read_users():
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Cadastrou não boy'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
@@ -45,19 +45,19 @@ def update_user(user_id: int, user: UserSchema):
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Esse caba não existe'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     del database[user_id - 1]
 
-    return {'message': 'Descadastrado com sucesso'}
+    return {'message': 'User deleted'}
 
 
 @app.get('/users/{user_id}', response_model=UserPublic)
 def read_user__exercicio(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Não Encontrado'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     return database[user_id - 1]
